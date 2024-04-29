6f09fbfe826f13ce2a09ea38127bb4bc750e3599
Sean Merz <smmerz56@yahoo.com>
2024-04-29 12:59:09 -0700

    The final version

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-use colored::*;
-use rand::Rng;
 use std::io;
-
+use rand::Rng;
+use colored::*;
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(0..=10);
 
-    println!("Welcome to a Guessing Game!\n");
+    let secret_num = rand::thread_rng().gen_range(1..=10);
 
-    loop {
-        println!("Please enter a number between 0 and 10:");
+    println!("Welcome to the guessing game!");
 
-        let mut guess = String::new();
+    'game_loop: loop {
+        println!("Please enter a number between 0 and 10: ");
+
+        let mut user_input = String::new();
 
         io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read input!");
+            .read_line(&mut user_input)
+            .expect("Failed to read number");
 
-        let guessed_number = match guess.trim().parse::<u32>() {
+        let guessed_num = match user_input.trim().parse::<u32>(){
             Ok(num) => num,
             Err(_) => return,
         };
 
-        if guessed_number == secret_number {
-            println!("{}", "Your guess is correct".blue());
-            break;
+        println!("You guessed: {guessed_num}");
+
+        // println!("Your guess is correct {}", guessed_num == secret_num);
+        if guessed_num == secret_num {
+            println!("Your guess is correct");
+            println!("{}","Your guess is correct".green());
+            break 'game_loop;
         } else {
-            if secret_number < guessed_number {
-                println!("{}", "Your number is too big! Guess again!".red());
-            } else {
-                println!("{}", "Your number is too small! Guess again!".red());
+            if guessed_num < secret_num {
+                println!("{}","Your guess is too low!".red());
+            }else{
+                println!("{}","Your guess is too high".red());
             }
+            
         }
     }
 }
